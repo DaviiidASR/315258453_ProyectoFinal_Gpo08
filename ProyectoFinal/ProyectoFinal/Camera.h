9b947f644d5647d9981045d07f2046143a7c6d6a
DaviiidASR <slowedskink40@hotmail.com>
2022-05-07 14:39:10 -0500

    315258453_ProyectoFinalVF1_Go08

--- ProyectoFinal/ProyectoFinal/Camera.h
+++ ProyectoFinal/ProyectoFinal/Camera.h
@@ -24,7 +24,7 @@ const GLfloat YAW = -90.0f;
 const GLfloat PITCH = 0.0f;
 const GLfloat SPEED = 6.0f;
 const GLfloat SENSITIVTY = 0.25f;
-const GLfloat ZOOM = 45.0f;
+const GLfloat ZOOM = 120.0f;
 
 // An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
 class Camera
@@ -55,18 +55,6 @@ public:
 	{
 		return glm::lookAt(this->position, this->position + this->front, this->up);
 	}
-
-	void Recorrido(GLfloat xOffset)//Modifica la rotación recibiendo el ángulo
-	{
-		this->yaw = xOffset;
-		this->updateCameraVectors();
-	}
-
-	void MovimientoAutomatico(GLfloat velocidad) //Realiza un movimiento automatico hacia adelante
-	{
-		this->position += this->front * velocidad;
-	}
-
 
 	// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
 	void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime)
@@ -121,7 +109,6 @@ public:
 		this->updateCameraVectors();
 	}
 
-	
 	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
 	void ProcessMouseScroll(GLfloat yOffset)
 	{
